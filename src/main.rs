57c32de7f461e4b62a9b198da81918ad3eaafd36
Tomasz Tracz <t@tracz.me>
2022-12-26 17:35:36 +0000

    Use all logical cores of cpu

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,17 @@ use num::Complex;
 use std::env;
 use std::fs::File;
 use std::str::FromStr;
+extern crate num_cpus;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() != 5 {
+        eprintln!("Usage: {} FILE PIXELS UPPERLEFT LOWERRIGHT", args[0]);
+        eprintln!(
+            "Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20",
+            args[0]
+        );
         std::process::exit(1);
     }
 
@@ -16,22 +22,25 @@ fn main() {
     let upper_left = parse_complex(&args[3]).expect("error parsing upper left");
     let lower_right = parse_complex(&args[4]).expect("error parsing lower right value");
 
-    let mut pixels = vec![0; bounds.0 * bounds.1];
+    let columns = bounds.0;
+    let rows = bounds.1;
 
-    let threads = 8;
-    let rows_per_band = bounds.1 / threads + 1;
+    let mut pixels = vec![0; columns * rows];
+
+    let threads = num_cpus::get();
+    let rows_per_band = rows / threads + 1;
 
     {
-        let bands: Vec<&mut [u8]> = pixels.chunks_mut(rows_per_band * bounds.0).collect();
+        let bands: Vec<&mut [u8]> = pixels.chunks_mut(rows_per_band * columns).collect();
 
         crossbeam::scope(|spawner| {
             for (i, band) in bands.into_iter().enumerate() {
                 let top = rows_per_band * i;
-                let height = band.len() / bounds.0;
-                let band_bounds = (bounds.0, height);
+                let height = band.len() / columns;
+                let band_bounds = (columns, height);
                 let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
                 let band_lower_right =
-                    pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
+                    pixel_to_point(bounds, (columns, top + height), upper_left, lower_right);
 
                 spawner.spawn(move |_| {
                     render(band, band_bounds, band_upper_left, band_lower_right);
